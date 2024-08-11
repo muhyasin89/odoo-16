a751d624c88c5bbbe9f653f82cdb1b88512cdc78
muhyasin <muhyasin89@gmail.com>
2024-08-11 12:34:13 +0700

    [main]add inherit form

--- owl/static/src/components/view_inheritance/res_partner_kanban_view.js
+++ owl/static/src/components/view_inheritance/res_partner_kanban_view.js
@@ -1,19 +1,27 @@
-/** @odoo-module **/
+/** @odoo-module */
 
 import { registry } from "@web/core/registry"
 import { kanbanView } from "@web/views/kanban/kanban_view"
 import { KanbanController } from "@web/views/kanban/kanban_controller"
 import { useService } from "@web/core/utils/hooks"
 
-class ResPartnerKanbanController  extends KanbanController {
+const { onWillStart } = owl
+
+class ResPartnerKanbanController extends KanbanController {
     setup(){
         super.setup()
-        console.log("This is res patner kanban  controller")
+        console.log("This is res partner kanban controller")
         this.action = useService("action")
+        this.orm = useService("orm")
+
+        onWillStart(async ()=>{
+            this.customerLocations = await this.orm.readGroup("res.partner", [], ['state_id'], ['state_id'])
+            console.log(this.customerLocations)
+        })
     }
 
     openSalesView(){
-        console.log("Open Sales View")
+        console.log("Open Sales view")
         this.action.doAction({
             type: "ir.actions.act_window",
             name: "Customer Sales",
@@ -21,13 +29,26 @@ class ResPartnerKanbanController  extends KanbanController {
             views: [[false, "list"], [false, "form"]]
         })
     }
+
+    selectLocations(state){
+        const id = state[0]
+        const name = state[1]
+
+        this.env.searchModel.setDomainParts({
+            state: {
+                domain: [['state_id', '=', id]],
+                facetLabel: name
+            }
+        })
+    }
 }
 
-export const resPartnerKanbanView   = {
+ResPartnerKanbanController.template = "owl.ResPartnerKanbanView"
+
+export const resPartnerKanbanView = {
     ...kanbanView,
-    Controller: ResPartnerKanbanController ,
-    buttonTemplate: "owl.ResPartnerKanbanView.Buttons"
+    Controller: ResPartnerKanbanController,
+    buttonTemplate: "owl.ResPartnerKanbanView.Buttons",
 }
-
 
 registry.category("views").add("res_partner_kanban_view", resPartnerKanbanView)
